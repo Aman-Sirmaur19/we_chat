25beb4cdb2ed5d23aa32da957083f7a31c6c26ee
Aman-Sirmaur19 <amansirmaur190402@gmail.com>
2024-02-18 02:54:53 +0530

    commit

--- lib/widgets/chat_user_card.dart
+++ lib/widgets/chat_user_card.dart
@@ -1,6 +1,6 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 import '../main.dart';
 import '../models/chat_user.dart';
